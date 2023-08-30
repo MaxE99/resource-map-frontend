ff04a89abbe55113f574a1d99effbd0247c1f0b6
MaxE99 <me-99@live.de>
2023-08-30 21:29:58 +0200

    refa(frontend): add bounds to map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -6,6 +6,10 @@ import { API } from "../../config";
 
 const Map = (): JSX.Element => {
   const [countries, setCountries] = useState<any>();
+  const bounds: [[number, number], [number, number]] = [
+    [-90, -180], // Bottom left corner of the world
+    [90, 180], // Top right corner of the world
+  ];
 
   useEffect(() => {
     fetch(API.GEOJSON, {
@@ -21,8 +25,11 @@ const Map = (): JSX.Element => {
         style={{ height: "50rem", borderRadius: "20px" }}
         center={[30, 0]}
         zoom={2}
+        bounds={bounds}
+        maxBoundsViscosity={1.0}
       >
         <TileLayer
+          noWrap={true}
           attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
