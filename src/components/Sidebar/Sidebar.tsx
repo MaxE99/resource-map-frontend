11ec4fefe0450fee4b7b1f0ffe92c96bf3dec82e
MaxE99 <me-99@live.de>
2023-09-19 20:06:47 +0200

    feat: add site information placeholders (#15)

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,13 +1,24 @@
-import { CSSProperties } from "react";
+import { CSSProperties, Fragment, useState } from "react";
+import { AiFillQuestionCircle } from "react-icons/ai";
+import { FcAbout } from "react-icons/fc";
+import { FaRegNewspaper } from "react-icons/fa";
 
 import PricePlot from "./PricePlot";
 import AccordionWrapper from "./AccordionWrapper";
 import { AccordionWrapperT, SidebarT } from "../../types/sidebar";
 import { SIDEBAR_STYLE } from "../../styles/sidebar";
 import { BASE_STYLE } from "../../styles/base";
+import "../../styles/sidebar.css";
 import { DOMAIN } from "../../config";
+import AboutUs from "./AboutUs";
+import Guide from "./Guide";
+import DataSources from "./DataSources";
 
 const Sidebar = ({ commodity, govInfo, prices }: SidebarT): JSX.Element => {
+  const [isAboutUsVisible, setIsAboutUsVisible] = useState<boolean>(false);
+  const [isGuideVisible, setIsGuideVisible] = useState<boolean>(false);
+  const [isDataSourcesVisible, setIsDataSourcesVisible] =
+    useState<boolean>(false);
   const govInfoData: AccordionWrapperT[] = [
     {
       index: 3,
@@ -32,52 +43,98 @@ const Sidebar = ({ commodity, govInfo, prices }: SidebarT): JSX.Element => {
         boxShadow: BASE_STYLE.BOX_SHADOW,
       }}
     >
-      <div style={{ padding: "20px", lineHeight: 1.8, textAlign: "justify" }}>
-        <div style={SIDEBAR_STYLE.NAME_CONTAINER as CSSProperties}>
-          <img
-            src={DOMAIN + "/static/" + commodity.img_path + ".jpg"}
-            alt={commodity.name + " Image"}
-            style={{
-              marginRight: "10px",
-              width: "40px",
-              borderRadius: "4px",
-            }}
-          />
-          {commodity.name}
-        </div>
-        {prices && prices.length > 0 && <PricePlot data={prices} />}
-        <AccordionWrapper
-          index={1}
-          summary="Overview"
-          details={
-            <div
-              style={{ margin: "20px 0 0" }}
-              dangerouslySetInnerHTML={{
-                __html: commodity.info
-                  .replace(/\n/g, "<br>")
-                  .replace(/(\s*<br\s*\/?>)+\s*$/, ""),
-              }}
-            ></div>
-          }
-        />
-        <AccordionWrapper
-          index={2}
-          summary="Largest Producers"
-          details={commodity.companies.map((commodity: string) => (
-            <div key={commodity} style={{ margin: "5px 0 0 10px" }}>
-              {commodity}
+      <div
+        style={{
+          padding: "20px",
+          lineHeight: 1.8,
+          textAlign: "justify",
+          minHeight: "calc(100vh - 104px)",
+        }}
+      >
+        {!isAboutUsVisible && !isGuideVisible && !isDataSourcesVisible && (
+          <Fragment>
+            <div style={SIDEBAR_STYLE.NAME_CONTAINER as CSSProperties}>
+              <img
+                src={DOMAIN + "/static/" + commodity.img_path + ".jpg"}
+                alt={commodity.name + " Image"}
+                style={{
+                  marginRight: "10px",
+                  width: "40px",
+                  borderRadius: "4px",
+                }}
+              />
+              {commodity.name}
             </div>
-          ))}
-        />
-        {govInfo?.events &&
-          govInfoData.map((item: AccordionWrapperT) => (
+            {prices && prices.length > 0 && <PricePlot data={prices} />}
             <AccordionWrapper
-              key={item.index}
-              index={item.index}
-              summary={item.summary}
-              details={item.details}
+              index={1}
+              summary="Overview"
+              details={
+                <div
+                  style={{ margin: "20px 0 0" }}
+                  dangerouslySetInnerHTML={{
+                    __html: commodity.info
+                      .replace(/\n/g, "<br>")
+                      .replace(/(\s*<br\s*\/?>)+\s*$/, ""),
+                  }}
+                ></div>
+              }
             />
-          ))}
+            <AccordionWrapper
+              index={2}
+              summary="Largest Producers"
+              details={commodity.companies.map((commodity: string) => (
+                <div key={commodity} style={{ margin: "5px 0 0 10px" }}>
+                  {commodity}
+                </div>
+              ))}
+            />
+            {govInfo?.events &&
+              govInfoData.map((item: AccordionWrapperT) => (
+                <AccordionWrapper
+                  key={item.index}
+                  index={item.index}
+                  summary={item.summary}
+                  details={item.details}
+                />
+              ))}
+          </Fragment>
+        )}
+        {isAboutUsVisible && <AboutUs />}
+        {isGuideVisible && <Guide />}
+        {isDataSourcesVisible && <DataSources />}
+      </div>
+      <div className="infoContainer">
+        <button
+          onClick={() => {
+            setIsAboutUsVisible(!isAboutUsVisible);
+            setIsGuideVisible(false);
+            setIsDataSourcesVisible(false);
+          }}
+        >
+          <FcAbout style={{ background: "white" }} />
+          <span>About Us</span>
+        </button>
+        <button
+          onClick={() => {
+            setIsGuideVisible(!isGuideVisible);
+            setIsAboutUsVisible(false);
+            setIsDataSourcesVisible(false);
+          }}
+        >
+          <FaRegNewspaper />
+          <span>Guide</span>
+        </button>
+        <button
+          onClick={() => {
+            setIsDataSourcesVisible(!isDataSourcesVisible);
+            setIsAboutUsVisible(false);
+            setIsGuideVisible(false);
+          }}
+        >
+          <AiFillQuestionCircle />
+          <span>Data Sources</span>
+        </button>
       </div>
     </div>
   );
--- src/styles/sidebar.ts
+++ src/styles/sidebar.ts
@@ -24,6 +24,7 @@ const SIDEBAR_STYLE = {
     borderRadius: "8px",
     overflowY: "auto",
     border: "1px solid",
+    position: "relative",
   },
   NAME_CONTAINER: {
     fontSize: "30px",
