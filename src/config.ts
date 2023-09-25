60ac52dc1c5782aa63e47538656ba12bac4b0ea1
MaxE99 <me-99@live.de>
2023-09-25 20:38:37 +0200

    fix: api redirect issues (#22)

--- src/config.ts
+++ src/config.ts
@@ -3,15 +3,15 @@ import { MarksT } from "./types/base";
 const DOMAIN = "http://localhost:8000/";
 
 const API = {
-  COUNTRIES: DOMAIN + "api/countries",
-  COMMODITIES: DOMAIN + "api/commodities",
-  PRODUCTION: DOMAIN + "api/production",
-  RESERVES: DOMAIN + "api/reserves",
-  GOV_INFO: DOMAIN + "api/gov_info",
-  IMPORTS: DOMAIN + "api/imports",
-  EXPORTS: DOMAIN + "api/exports",
-  PRICES: DOMAIN + "api/prices",
-  BALANCE: DOMAIN + "api/balance",
+  COUNTRIES: DOMAIN + "api/countries/",
+  COMMODITIES: DOMAIN + "api/commodities/",
+  PRODUCTION: DOMAIN + "api/production/",
+  RESERVES: DOMAIN + "api/reserves/",
+  GOV_INFO: DOMAIN + "api/gov_info/",
+  IMPORTS: DOMAIN + "api/imports/",
+  EXPORTS: DOMAIN + "api/exports/",
+  PRICES: DOMAIN + "api/prices/",
+  BALANCE: DOMAIN + "api/balance/",
 } as const;
 
 const MARKS: MarksT[] = [
