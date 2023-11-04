33dc64cf9c09670026198bc11b4fe0ab70392560
Kilian <kilianc0709@gmail.com>
2023-11-04 16:24:44 +0100

    fix: set dictonary for centroids that could not be numerically computed

--- src/components/Map/functions.ts
+++ src/components/Map/functions.ts
@@ -2,8 +2,20 @@ import { LatLngExpression } from "leaflet";
 import { centroid, polygon, multiPolygon } from "@turf/turf";
 
 const calculateStrongholdCoords = (
-  feature: GeoJSON.Feature
+  feature: GeoJSON.Feature,
 ): LatLngExpression => {
+  const centroidAdjustment: { [key: string]: number[] } = {
+    "United States": [39.149389, -99.94949],
+    Canada: [57.347269, -102.9533],
+    Norway: [61.774301, 9.482824],
+  };
+  if (
+    Object.keys(centroidAdjustment).find(
+      (entry) => entry === feature.properties?.ADMIN,
+    )
+  ) {
+    return centroidAdjustment[feature.properties?.ADMIN] as LatLngExpression;
+  }
   let turfedCoordinates: any = [];
   if (feature.geometry.type === "Polygon") {
     turfedCoordinates = polygon(feature.geometry.coordinates);
